dba1220beebf47d52fd0de5c951fd1c6b064cb72
TJ Klint <timothyjklint@gmail.com>
2024-06-27 23:19:33 -0400

    Running bare minimum to get the server up.

--- app/app.py
+++ app/app.py
@@ -1,19 +1,14 @@
-import os 
-import numpy as numpy
-import tensorflow as tf 
+from flask import Flask
 
-from flask import Flask, flash, request, make_response, render_template
-
-port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
 @app.route("/test", methods=['GET', 'POST'])
 def test():
     return jsonify({{hey: "please work"}})
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    return jsonify({{hey: "please work"}})
+    return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+    app.run()
