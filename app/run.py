87deb137dc3fab71dbd82411a8591dada28ee70b
TJ Klint <timothyjklint@gmail.com>
2024-06-27 22:36:39 -0400

    Added index.

--- app/run.py
+++ app/run.py
@@ -7,8 +7,12 @@ from flask import Flask, flash, request, make_response, render_template
 app = Flask(__name__)
 
 @app.route("/test", methods=['GET', 'POST'])
-def index():
+def test():
     return "TESTINGGGGG"  
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "INDEXXXXXXXXXX"
 
 if __name__ == "__main__":
     app.run(debug=True)
