050b2febc25bd7e8a96a312391a503297ba3ee74
TJ Klint <timothyjklint@gmail.com>
2024-06-27 22:30:45 -0400

    Changes to see if testing will work.

--- app/run.py
+++ app/run.py
@@ -1,18 +1,14 @@
 import os 
 import numpy as numpy
-from PIL import Image
 import tensorflow as tf 
 
-from resizeimage import resizeimage 
-from werkzeug import secure_filename
-from keras_preprocessing import image 
 from flask import Flask, flash, request, make_response, render_template
 
 app = Flask(__name__)
 
 @app.route("/test", methods=['GET', 'POST'])
 def index():
-    return {"test" : "success :p"}
+    return "TESTINGGGGG"  
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
