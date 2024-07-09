51e93cf41bffed7152ea726b831f0a14f3be0a9a
TJ Klint <timothyjklint@gmail.com>
2024-07-09 19:26:11 -0400

    Revert "Fixing file hierachy for testing purposes."

--- app/app.py
+++ app/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    return jsonify({{hey: "please work"}})
+
 @app.route("/")
 def index():
-    return "Hello, this is your Flask app running on Heroku!"
+    return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
