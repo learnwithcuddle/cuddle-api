a2d3aefea755b25317e69de79fc47b749176d89a
TJ Klint <167789978+woahNukaALT@users.noreply.github.com>
2024-07-09 14:22:32 -0400

    Testing heroku server with primative sign verification.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from verify_sign import predict
 
 app = Flask(__name__)
 
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    return jsonify({{hey: "please work"}})
+@app.route('/verify_sign', methods=['POST'])
+def process_image():
+    result = predict()
+    return result
 
 @app.route("/")
 def index():
     return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
