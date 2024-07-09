df483733641b5183ac7a3738d530d94ffeb15a0b
TJ Klint <timothyjklint@gmail.com>
2024-07-09 17:50:43 -0400

    Debug for API.

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify
+#from flask import Flask, request, jsonify
 #from app.services.verify_sign import predict
+
+#app = Flask(__name__)
+
+#@app.route('/verify_sign', methods=['POST'])
+#def process_image():
+#    result = 1
+#    #result = predict()
+#    return result
+
+#@app.route("/")
+#def index():
+#    return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/verify_sign', methods=['POST'])
-def process_image():
-    result = 1
-    #result = predict()
-    return result
+@app.route('/')
+def home():
+    return "The flask app running on Heroku!"
 
-@app.route("/")
-def index():
-    return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
