f5c602052ce07d9ff9d0fcf5ba1f268e8dffc1b0
TJ Klint <timothyjklint@gmail.com>
2024-07-09 17:59:36 -0400

    Another attempted debug.

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-#from flask import Flask, request, jsonify
-#from app.services.verify_sign import predict
-
-#app = Flask(__name__)
-
-#@app.route('/verify_sign', methods=['POST'])
-#def process_image():
-#    result = 1
-#    #result = predict()
-#    return result
-
-#@app.route("/")
-#def index():
-#    return "FLASK APP IS WORKING YIPEEEEEEEE :333333"
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "The flask app running on Heroku!"
+    return "Hello, this is your Flask app running on Heroku!"
 
 if __name__ == '__main__':
     app.run()
